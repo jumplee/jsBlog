d810da5bcf3d7b944e7e3725e4c35d13b7febbff
lee <liyuntao09@gmail.com>
2015-08-12 22:58:46 +0800

    Initial commit

---
+++ App/Lib/Controller/Admin/BlogController.js
@@ -0,0 +1,23 @@
+/**
+ * controller
+ * @return 
+ */
+module.exports = Controller("Home/BaseController", function(){
+  "use strict";
+  return {
+    indexAction: function(){
+      //render View/Home/index_index.html file
+      this.display();
+    },
+      addAction: function(){
+          //render View/Home/index_index.html file
+          this.display();
+      },
+      addSaveAction:function(){
+          this.json({
+              name:'222'
+          });
+      }
+
+  };
+});
