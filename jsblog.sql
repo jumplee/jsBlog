691cd5b78ca9ea1a3597f6bf54700713b4819c07
jumplee <liyuntao09@gmail.com>
2015-08-23 19:24:04 +0800

    sql 备份

--- jsblog.sql
+++ jsblog.sql
@@ -1,64 +1,64 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : local
-Source Server Version : 50090
-Source Host           : localhost:3307
-Source Database       : jsblog
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50538
+ Source Host           : localhost
+ Source Database       : jsblog
 
-Target Server Type    : MYSQL
-Target Server Version : 50090
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50538
+ File Encoding         : utf-8
 
-Date: 2015-08-13 22:54:10
+ Date: 08/23/2015 19:23:52 PM
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for js_blog
+--  Table structure for `js_blog`
 -- ----------------------------
 DROP TABLE IF EXISTS `js_blog`;
 CREATE TABLE `js_blog` (
-  `id` int(11) NOT NULL auto_increment,
-  `title` varchar(255) default NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
   `content` text,
-  `add_time` timestamp NULL default NULL on update CURRENT_TIMESTAMP,
-  `category` int(11) default NULL,
-  PRIMARY KEY  (`id`)
+  `add_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `category` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of js_blog
--- ----------------------------
-
--- ----------------------------
--- Table structure for js_category
+--  Table structure for `js_category`
 -- ----------------------------
 DROP TABLE IF EXISTS `js_category`;
 CREATE TABLE `js_category` (
-  `id` int(11) NOT NULL auto_increment,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL COMMENT '分类名称',
-  `description` varchar(300) default NULL COMMENT '分类描述',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `description` varchar(300) DEFAULT NULL COMMENT '分类描述',
+  `pid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of js_category
+--  Records of `js_category`
 -- ----------------------------
+BEGIN;
+INSERT INTO `js_category` VALUES ('1', '默认', '默认分类', '0');
+COMMIT;
 
 -- ----------------------------
--- Table structure for js_images
+--  Table structure for `js_images`
 -- ----------------------------
 DROP TABLE IF EXISTS `js_images`;
 CREATE TABLE `js_images` (
-  `id` int(11) NOT NULL auto_increment,
-  `blog_id` int(11) default NULL,
-  `url` varchar(1000) default NULL,
-  PRIMARY KEY  (`id`),
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `blog_id` int(11) DEFAULT NULL,
+  `url` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `bog_relate` (`blog_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of js_images
--- ----------------------------
+SET FOREIGN_KEY_CHECKS = 1;
